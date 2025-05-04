8f7ea0f47efe06bd5f382cc4024f1e6d296aae64
Esra Şahin <sahiinesra66@gmail.com>
2025-05-04 14:29:03 +0300

    setup: install NativeWind & Tailwind, update babel.config.js, metro.config.js, global.css

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -2,6 +2,8 @@ import { Tabs } from 'expo-router';
 import React from 'react';
 import { Platform } from 'react-native';
 
+import './globals.css';
+
 import { HapticTab } from '@/components/HapticTab';
 import { IconSymbol } from '@/components/ui/IconSymbol';
 import TabBarBackground from '@/components/ui/TabBarBackground';
